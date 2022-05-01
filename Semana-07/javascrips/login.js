a502b4250c2e25c2bb63679896abcc550eafa085
Agustin <alchazarreta0@gmail.com>
2022-05-01 13:18:05 -0300

    BaSP-W07-2022: login finished

--- Semana-07/javascrips/login.js
+++ Semana-07/javascrips/login.js
@@ -2,27 +2,8 @@
 let inputEmail = document.getElementById('input-email');
 let inputPass = document.getElementById('input-pass');
 let btn = document.getElementById('login-btn');
-//fetchs
-// var userInfo = {
-//   email: inputEmail.value,
-//   password: inputPass.value,
-// };
-// const URL_LOGIN = `https://basp-m2022-api-rest-server.herokuapp.com/login`;
-// fetch(URL_LOGIN),
-//   {
-//     method: 'POST',
-//     body: JSON.stringify(userInfo),
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   }
-
-//     .then((response) => response.json())
-//     .then((data) => console.log(data.errors))
-//     .catch((e) => {
-//       console.log(e);
-//     });
-
+let messageEmail = document.getElementById('output-email');
+let messagePass = document.getElementById('output-pass');
 let values = [
   {
     boolean: false,
@@ -43,17 +24,16 @@ function validateEmail(e) {
 }
 
 function checkEmail(e) {
-  let message = document.getElementById('output-email');
   var result = validateEmail(e);
   if (result) {
     document.getElementById('output-email').innerHTML = 'Valid Email';
-    message.style.visibility = 'visible';
-    message.style.backgroundColor = '#aace9b';
+    messageEmail.style.visibility = 'visible';
+    messageEmail.style.backgroundColor = '#aace9b';
     values[0].boolean = true;
   } else {
     document.getElementById('output-email').innerHTML = 'Not a valid Email';
-    message.style.visibility = 'visible';
-    message.style.backgroundColor = '#cc5050';
+    messageEmail.style.visibility = 'visible';
+    messageEmail.style.backgroundColor = '#cc5050';
     values[0].boolean = false;
   }
 }
@@ -63,23 +43,23 @@ function eliminateMsgEmail() {
 }
 function validatePass(e) {
   let password = e.target.value;
-  let message = document.getElementById('output-pass');
+
   let stringNumbers = '0123456789';
-  let stringLetters = 'abcdefghijklmnopqrstuvwxyz';
+  let stringLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let stringLettersAndNumbers = stringNumbers + stringLetters;
-  let arrPass = password.toLowerCase().split('');
+  let arrPass = password.split('');
   let includesNumbers = arrPass.some((c) => stringNumbers.includes(c));
   let includesLetters = arrPass.some((c) => stringLetters.includes(c));
   let isValidChar = arrPass.every((c) => stringLettersAndNumbers.includes(c));
   if (includesNumbers && includesLetters && isValidChar) {
     document.getElementById('output-pass').innerHTML = 'Valid Pass';
-    message.style.visibility = 'visible';
-    message.style.backgroundColor = '#aace9b';
+    messagePass.style.visibility = 'visible';
+    messagePass.style.backgroundColor = '#aace9b';
     values[1].boolean = true;
   } else {
     document.getElementById('output-pass').innerHTML = 'Invalid password';
-    message.style.visibility = 'visible';
-    message.style.backgroundColor = '#cc5050';
+    messagePass.style.visibility = 'visible';
+    messagePass.style.backgroundColor = '#cc5050';
     values[1].boolean = false;
   }
 }
@@ -94,47 +74,33 @@ inputEmail.addEventListener('focus', eliminateMsgEmail);
 inputPass.addEventListener('focus', eliminateMsgPass);
 btn.addEventListener('click', function logIn(e) {
   e.preventDefault();
-  var userInfo = {
-    email: inputEmail.value,
-    password: inputPass.value,
-  };
-  let query = Object.keys(userInfo)
-    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(userInfo[k]))
-    .join('&');
-  const URL_LOGIN =
-    `https://basp-m2022-api-rest-server.herokuapp.com/login?` + query;
-  fetch(URL_LOGIN, {
-    method: 'POST',
-    body: JSON.stringify(userInfo),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(function (response) {
-      return response.text();
-    })
-    .then(function (text) {
-      console.log(text);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  let arr = [];
+  let errArr = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i].boolean) {
+      arr.push(values[i].boolean);
+    } else {
+      errArr.push(values[i].errorType);
+    }
+  }
+  if (arr.length === 2) {
+    fetch(
+      `https://basp-m2022-api-rest-server.herokuapp.com/login?email=${inputEmail.value}&password=${inputPass.value}`
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        alert(
+          'Email : ' +
+            inputEmail.value +
+            '\n' +
+            'Password : ' +
+            inputPass.value +
+            '\n' +
+            data.msg
+        )
+      )
+      .catch((error) => {
+        alert(error);
+      });
+  } else alert(errArr);
 });
-
-// function alerta(e) {
-//   e.preventDefault();
-//   let arr = [];
-//   let errArr = [];
-//   for (let i = 0; i < values.length; i++) {
-//     if (values[i].boolean) {
-//       arr.push(values[i].boolean);
-//     } else {
-//       errArr.push(values[i].errorType);
-//     }
-//   }
-//   if (arr.length === 2) {
-//     alert(
-//       'Email : ' + inputEmail.value + '\n' + 'Password : ' + inputPass.value
-//     );
-//   } else alert(errArr);
-// }
